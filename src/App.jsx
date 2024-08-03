e1229b5647564166d9d74ab6927f99280683a935
PrithviS18 <prithvisen003@gmail.com>
2024-08-03 11:08:31 +0530

    configuring routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
-import {
-    Routes, Route
-} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Profile from "./Profile.jsx"
+import Home from './Home.jsx';
 
 function App() {
     return (
         <>
             <Routes>
+                <Route exact path="/" element={<Home/>} />
                 <Route exact path="/profile/:id" element={<Profile />} />
             </Routes>
         </>
